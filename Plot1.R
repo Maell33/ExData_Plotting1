2694aba813a59f5b99f33e48d939a1e82e042a57
Maelle Mairesse <103257265+Maell33@users.noreply.github.com>
2022-07-22 14:14:30 +0200

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,14 +1,14 @@
 #### Script to obtain plot 1
 
-# Read and subsetting data: 
+# Read and subsetting data:
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-data <- read.table(unz(temp,"household_power_consumption.txt"), sep=";")
+data <- read.table(unz(temp,"household_power_consumption.txt"), sep=";",header=T,na="?",
+                   colClasses=c("character","character",
+                                "numeric","numeric","numeric","numeric","numeric","numeric","numeric"))
 head(data)
-colnames(data) <- data[1,] # rename column header
-data <- data[-1,]
+#subset data
 subdata <- subset(data,data$Date=="1/2/2007"|data$Date=="2/2/2007")
-subdata$Global_active_power <- as.numeric(subdata$Global_active_power)
 
 # Create histogram and save it as png: 
 png(file="Plot1.png",width=480,height = 480)
